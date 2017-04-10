6c6fa29aefe3eaa25b742a86278fccf8c74eb89f
jjunji <jh.jun92@gmail.com>
2017-04-10 16:46:17 +0900

    complete

---
+++ python/notebook/polymorphism/character.py
@@ -0,0 +1,18 @@
+# 같은 코드 다른 결과 -> 폴리모르피즘?
+# abstract_class : 객체를 만들 수 없는 클래스
+# 기본 클래스, 부모 클래스의 역할.
+
+from abc import * # abstract base class
+
+class Character(metaclass = ABCMeta):
+    def __init__(self):
+        self.hp = 100
+        self.attack_power = 20
+       
+    def attack(self, other, attack_kind):
+        other.get_damage(self.attack_power, attack_kind)
+        
+    @abstractmethod # 객체를 만들 수 없는 클래스이므로, abstract메소드를 가져야 인스턴스 생성 가능? -> abstract메소드를 꼭 쓰게 할 때.
+    def get_damage(self, attack_power, attack_kind):
+        pass
+    
